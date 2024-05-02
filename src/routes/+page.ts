359806385791040c02279fcf387fc4c54e6cd709
Wonwoo Seo <wonwooseo@hotmail.com>
2024-05-03 00:27:05 +0900

    apply api changes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,18 @@
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ fetch }) => {
-  const price = await fetch('/api/price');
-  const trend = await fetch('/api/price/trend?window=week');
-  const regional = await fetch('/api/price/region?region_code=0000')
+  const respPrice = await fetch('/api/price');
+  const price = await respPrice.json();
+
+  const respTrend = await fetch('/api/price/trend?window=week');
+  const trend = await respTrend.json();
+
+  const respRegional = await fetch(`/api/price/region?region_code=0000&date_unix=${price.date_unix}`);
+  const regional = await respRegional.json();
 
   return {
-    price: await price.json(),
-    trend: await trend.json(),
-    regional: await regional.json(),
+    price: price,
+    trend: trend,
+    regional: regional,
   }
 }
--- src/routes/api/price/region/+server.ts
+++ src/routes/api/price/region/+server.ts
@@ -3,5 +3,6 @@ import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({ url }) => {
   const regionCode = url.searchParams.get('region_code');
-  return await fetch(`${API_URL}/price/region?region_code=${regionCode}`);
+  const dateUnix = url.searchParams.get('date_unix');
+  return await fetch(`${API_URL}/price/region?region_code=${regionCode}&date_unix=${dateUnix}`);
 }
